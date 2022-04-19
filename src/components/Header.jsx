e3749cb0b3076e0bb317c044a1ee55ad615721e0
Shaan3110 <hitarasigan0987654321@gmail.com>
2022-04-19 23:28:59 +0530

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "../stylesheets/Header.css";
+import logo from '../assets/images/logo.png'
 
 const Header = () => {
   return (
     <div className="heading">
-      <div className="logo"></div>
+      <div className="logo">
+          <img src={logo} alt="Efest" />
+      </div>
       <div className="lefticons">
         <h1>What's in it?</h1>
         <button>Sign Up</button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Section1 from '../components/Section1'
 
 const Home = () => {
   return (
     <>
         <Header/>
+        <Section1/>
     </>
   )
 }
