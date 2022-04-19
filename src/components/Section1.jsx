5e257e67d4a827844adecc667b5406397bb6850e
Shaan3110 <hitarasigan0987654321@gmail.com>
2022-04-19 23:28:04 +0530

    header made

---
+++ src/components/Section1.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../stylesheets/Section1.css";
+
+const Section1 = () => {
+  return (
+    <div className="section1">
+      <div className="texts">
+        <h1>Spark E-Cell presents</h1>
+        <h1>Entrepreneurship <br/>Festival - 2022</h1>
+        <h1>Bennett University</h1>
+        <div className="events">
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section1;
